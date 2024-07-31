cf2d718d9323d6aeafb936142174957978a8b01a
kchristmas <kirstinchristmas@gmail.com>
2024-07-31 15:38:28 -0400

    update 2

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -36,7 +36,7 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-app.use("/api/gifts,giftRoutes")
+app.use("/api/gifts",giftRoutes)
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
         const collection = db.collection("gifts");
 
         // Task 3: Fetch all gifts using the collection.find method. Chain with toArray method to convert to JSON array
-        const gifts = db.find({}).toArray();
+        const gifts = await collection.find({}).toArray();
 
         // Task 4: return the gifts using the res.json method
         res.json(gifts);
