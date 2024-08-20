3672088c814f818b0c38bde91df0bfcd178f89f0
kchristmas <kirstinchristmas@gmail.com>
2024-08-20 14:34:42 -0400

    Update App.js

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
+import DetailsPage from "./components/DetailsPage/DetailsPage";
+import SearchPage from "./components/SearchPage/SearchPage";
+import Profile from "./components/Profile/Profile";
 import MainPage from './components/MainPage/MainPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
-    const navigate = useNavigate();
   return (
     <>
         <Navbar/>
@@ -17,6 +19,9 @@ function App() {
           <Route path="/app" element={<MainPage />} />
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
+          <Route path="/app/product/:productId" element={<DetailsPage />} />
+          <Route path="/app/search" element={<SearchPage />} />
+          <Route path="/app/profile" element={<Profile />} />
         </Routes>
         </>
   );
